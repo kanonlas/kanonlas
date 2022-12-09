fe635c3198ae5e65283f9a4b488f29f4a4a2e4f9
tumarai <kanonlas_r@cmu.ac.th>
2022-12-09 15:55:47 +0700

    lab6_1.cpp completed

--- lab6_1.cpp
+++ lab6_1.cpp
@@ -2,8 +2,21 @@
 using namespace std;
 
 int main(){
+    int odd = 0;
+    int num = 1;
+    int even = 0; 
+    while(num != 0){
     cout << "Enter an integer: ";
-    cout << "#Even numbers = ";
-    cout << "#Odd numbers = ";
+    cin  >> num;
+        if(num%2 == 0){
+      odd = odd+1;
+}else{
+ even = even + 1;
+}
+    }
+    cout << "#Even numbers = " << even << endl;
+    cout << "#Odd numbers = " <<  odd << endl ;
     return 0;
 }
+
+
